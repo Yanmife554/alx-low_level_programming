baac78c1fe1b19d1f11912bb8ee2598bca2f5618
Yanmife554 <doluwayanmife@gmail.com>
2023-03-22 17:56:38 -0700

    5. Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: number whose sign will be printed
+ * Return: 1 if n is greater zero, 0 if n is zero and -1 if n is
+ * less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -3,3 +3,4 @@ a function that prints the alphabet, in lowercase, followed by a new line.
 a function that prints 10 times the alphabet, in lowercase, followed by a new line.
 a function that checks for lowercase character.
 a function that checks for alphabetic character.
+a function that prints the sign of a number.
